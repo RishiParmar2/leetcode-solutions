1c50ae3a7596a0e66ecada0df4ec6f23d3d6f826
RishiParmar2 <146367007+RishiParmar2@users.noreply.github.com>
2026-08-08 13:19:11 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (7.52%) - LeetSync

--- DSA/724-find-pivot-index/find-pivot-index.c
+++ DSA/724-find-pivot-index/find-pivot-index.c
@@ -1,16 +1,22 @@
-int pivotIndex(int* nums, int numsSize) {
-    int idx = -1;
-    int leftsum = 0, rightsum = 0, sum = 0;
-    if (numsSize == 1)
-        return 0;
-    for (int i = 0; i < numsSize; i++) {
+int pivotIndex(int* nums, int numsSize)
+{
+    int leftsum = 0;
+    int sum = 0;
+
+    for (int i = 0; i < numsSize; i++)
+    {
         sum += nums[i];
     }
-    for (int i = 0; i < numsSize; i++) {
-        rightsum = sum - leftsum - nums[i];
+
+    for (int i = 0; i < numsSize; i++)
+    {
+        int rightsum = sum - leftsum - nums[i];
+
         if (leftsum == rightsum)
             return i;
+
         leftsum += nums[i];
     }
-    return idx;
+
+    return -1;
 }
