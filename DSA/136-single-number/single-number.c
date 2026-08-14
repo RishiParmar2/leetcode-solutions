e19f0c95f5f56e1fdbfb9e04cfe2c49e1eaedb3c
Rishi Parmar <146367007+RishiParmar2@users.noreply.github.com>
2026-08-14 10:15:33 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (93.44%) - LeetSync

--- DSA/136-single-number/single-number.c
+++ DSA/136-single-number/single-number.c
@@ -1,15 +1,19 @@
 int singleNumber(int* nums, int numsSize) {
-    int freq[60001] = {0};
+    // int freq[60001] = {0};
 
-    for (int i = 0; i < numsSize; i++) {
-        freq[nums[i] + 30000]++;
-    }
+    // for (int i = 0; i < numsSize; i++) {
+    //     freq[nums[i] + 30000]++;
+    // }
 
-    for (int i = 0; i < 60001; i++) {
-        if (freq[i] == 1) {
-            return i - 30000;
-        }
-    }
+    // for (int i = 0; i < 60001; i++) {
+    //     if (freq[i] == 1) {
+    //         return i - 30000;
+    //     }
+    // }
 
-    return 0;
+    int res = 0;
+    for(int i = 0; i< numsSize; i++)
+        res ^= nums[i];
+    return res;
+    // return 0;
 }
